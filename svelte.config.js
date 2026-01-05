a0e6245672d121541ac7020f858af9bf50636a7c
Luuk <147055722+Lutrian1@users.noreply.github.com>
2026-01-05 21:33:26 +0100

    veranderd van netlify naar static page sveltekit

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,16 @@
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// default options are shown
 		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		})
 	}
 };
